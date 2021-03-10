c2a7f172649539573bc17128803996ab391e2428
Brandon Kuczenski <brandon.kuczenski@301south.net>
2021-03-09 18:19:37 -0800

    Remove 'example' naming verbiage and improve exports

--- synonym_dict/__init__.py
+++ synonym_dict/__init__.py
@@ -1,5 +1,6 @@
-__version__ = (0, 1, 1)
-
 from .lower_dict import LowerDict
 from .synonym_dict import SynonymDict, MergeError, TermExists
 from .synonym_set import SynonymSet
+
+from .compartments import Compartment, CompartmentManager, InconsistentLineage, NonSpecificCompartment
+from .flowables import Flowable, FlowablesDict
